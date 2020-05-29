fbc7bf918f274df379076d8b68754e465378f362
Tristan Heuer <tristan.heuer@gmail.com>
2020-05-29 14:20:37 -0400

    Redirect root to university list

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ const router = new KoaRouter();
 
 router.get('/', async (ctx) => {
   await ctx.render('index', { appVersion: pkg.version });
+  ctx.redirect(ctx.router.url('universities.list'));
 });
 
 module.exports = router;
