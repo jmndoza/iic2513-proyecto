973b4884016a3e37ab84c73db5728e53c1de497c
Ignacio <inzuniga@uc.cl>
2020-06-30 23:15:01 -0400

    Api universities

---
+++ src/routes/api/index.js
@@ -0,0 +1,24 @@
+const KoaRouter = require('koa-router');
+// const jwt = require('koa-jwt');
+
+// const authApi = require('./auth');
+const universitiesApi = require('./universities');
+
+const router = new KoaRouter();
+
+// unauthenticated endpoints
+// router.use('/auth', authApi.routes());
+
+// JWT authentication without passthrough (error if not authenticated)
+/* router.use(jwt({ secret: process.env.JWT_SECRET, key: 'authData' }));
+router.use(async (ctx, next) => {
+  if (ctx.state.authData.userId) {
+    ctx.state.currentUser = await ctx.orm.user.findByPk(ctx.state.authData.userId);
+  }
+  return next();
+}); */
+
+// authenticated endpoints
+router.use('/universities', universitiesApi.routes());
+
+module.exports = router;
